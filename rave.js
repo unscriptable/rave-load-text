6dcd09177f77715d6ed928522892b8c399454e4b
unscriptable <john@unscriptable.com>
2014-05-02 13:43:49 -0400

    Update to rave 0.1's extension API.

--- rave.js
+++ rave.js
@@ -1,29 +1,28 @@
 /** @license MIT License (c) copyright 2014 original authors */
 /** @author Brian Cavalier */
 /** @author John Hann */
-var createFileExtFilter = require('rave/lib/createFileExtFilter');
-var overrideIf = require('rave/lib/overrideIf');
 var es5Transform = require('rave/lib/es5Transform');
 
-var defaultExtensions = [ 'text', 'html', 'txt', 'htm' ];
+var defaultExtensions = [ 'text', 'html', 'txt', 'htm', 'css' ];
 
 exports.create = create;
 exports.instantiate = instantiate;
 
 function create (context) {
-	var pipeline = {
-		instantiate: instantiate
-	};
-
 	// override extensions if supplied by dev
 	var extensions = 'loadText' in context
 		? context.loadText
 		: defaultExtensions;
 
 	return {
-		pipeline: function (loader) {
-			return overrideIf(createFileExtFilter(extensions), loader, pipeline);
-		}
+		load: [
+			{
+				extensions: extensions,
+				hooks: {
+					instantiate: instantiate
+				}
+			}
+		]
 	};
 
 }
